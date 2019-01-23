7edb07a8ff1032a9d2508287506c77fcf975d180
Mohaimanul Hoque Chowdhury <ramim@lamusica.com>
2019-01-23 13:49:52 -0500

    initial Commit

---
+++ dicees/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  dicees
+//
+//  Created by Mohaimanul Hoque Chowdhury on 1/23/19.
+//  Copyright © 2019 Mohaimanul Hoque Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var randomIndexNumberOne : Int = 0;
+    var randomIndexNumberTwo : Int = 0;
+    
+    let diceImageArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func rollDiceButton(_ sender: UIButton) {
+        randomIndexNumberOne = Int.random(in: 0 ... 5)
+        
+        randomIndexNumberTwo = Int.random(in: 0...5)
+        
+
+        
+        diceImageView1.image = UIImage(named: diceImageArray[randomIndexNumberOne])
+        
+        diceImageView2.image = UIImage(named: diceImageArray[randomIndexNumberTwo])
+    }
+    
+}
+
